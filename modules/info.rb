3d57705fa687f30a1b02498fe13dba092d9b1f81
Eclipse <0-Eclipse-0@users.noreply.github.com>
2016-11-09 03:58:21 +0800

    Added height and address

--- modules/info.rb
+++ modules/info.rb
@@ -2,4 +2,10 @@ module Info
   def Info.date(month, day, year)
     return "#{month}/#{day}/#{year}"
   end
+  def Info.height(feet, inches)
+    return "#{feet} feet, #{inches} inches"
+  end
+  def Info.address(number, name, type)
+    return "#{number} #{name}, #{type}."
+  end
 end
